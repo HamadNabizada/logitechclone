62185d7275e1d8455d50989bff993092361a7af8
Hamad <HamadNabizada@gmail.com>
2022-07-28 22:46:16 -0700

    clean up random text

--- components/Header.js
+++ components/Header.js
@@ -4,10 +4,7 @@ import style from '../styles/Header.module.css'
 export default function Header(){
     return(
         <div className={style.wrapper}>
-            Header
-            Header
-            Header
-            Header
+            
         </div>
     )
 }
