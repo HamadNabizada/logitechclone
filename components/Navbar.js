3b5ec5460c2cc9a15468f64e6468fd2473f80988
Hamad <HamadNabizada@gmail.com>
2022-08-04 20:55:06 -0700

    added navbar icons. navbar styling

--- components/Navbar.js
+++ components/Navbar.js
@@ -8,12 +8,12 @@ export default function Navbar(){
                 <div className={style.menuBtn}>
                     MenuBtn
                 </div>
-                <h2 className={style.logo}>
-                    LOGITECH G
+                <h2 className={style.logoWrapper}>
+                    <img className={style.logo} src="/icons/Logitech_logo.svg" alt="Logitech Logo" />
                 </h2>
                 <ul className={style.cartWrapper}>
-                    <li className={style.cartSearch}>Cart</li>
-                    <li className={style.cartSearch}>Search</li>
+                    <li className={style.cartSearch}><img className={style.icon} src="/icons/cart-g.svg" alt="Cart Icon" /></li>
+                    <li className={style.cartSearch}><img className={style.icon} src="/icons/search-g.svg" alt="Search Icon" /></li>
                 </ul>
             </div>
         </nav>
