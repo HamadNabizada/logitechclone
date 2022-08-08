71301b677f6d305e02ec1c275c33d642dbebb174
Hamad <HamadNabizada@gmail.com>
2022-08-08 07:20:19 -0700

    arrow animation added

--- components/Hero.js
+++ components/Hero.js
@@ -1,16 +1,25 @@
 import style from '../styles/Hero.module.css'
 import data from './heroData.js'
 import HeroCard from './HeroCard'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 
 export default function Hero(){
     let [currentCard, setCurrentCard] = useState(0)
-    console.log(data[currentCard].imgSource);
+
+    function nextBG(){
+        setCurrentCard(prevCurrentCard =>{
+            let newCurrentCard = prevCurrentCard + 1
+            if (newCurrentCard === data.length){
+                newCurrentCard = 0
+            }
+            return newCurrentCard
+        })
+    }
     return( 
         <div className={style.wrapper}>
             <div className={style.arrowsWrapper}>
-                <p className={style.arrow}>&lsaquo;</p>
-                <p className={style.arrow}>&rsaquo;</p>
+                <p className={`${style.arrow} ${style.arrowLeft}`}>&lsaquo;</p>
+                <p onClick={nextBG} className={`${style.arrow} ${style.arrowRight}`}>&rsaquo;</p>
             </div>
             <HeroCard
                 imgSource={data[currentCard].imgSource}
