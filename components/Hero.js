7c6d1b9b102dabf35a472a3b689fb8ae386d1d26
Hamad <HamadNabizada@gmail.com>
2022-08-07 23:53:41 -0700

    hero component created

---
+++ components/Hero.js
@@ -0,0 +1,10 @@
+import style from '../styles/Hero.module.css'
+
+
+export default function Hero(){
+    return( 
+        <div className={style.wrapper}>
+
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import Header from '../components/Header'
 import style from '../styles/Index.module.css'
 import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
 
 export default function Index() {
   return (
     <div className={style.wrapper}>
       <Header />
       <Navbar />
+      <Hero />
     </div>
   )
 }
