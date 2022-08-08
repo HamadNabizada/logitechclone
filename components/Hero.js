38993a14ba0797b344d4e8316cb7e282d0ce7068
Hamad <HamadNabizada@gmail.com>
2022-08-08 05:28:52 -0700

    added data for hero carousel

--- components/Hero.js
+++ components/Hero.js
@@ -1,10 +1,12 @@
 import style from '../styles/Hero.module.css'
-
+import data from './heroData.js'
 
 export default function Hero(){
+
+    console.log(data);
     return( 
         <div className={style.wrapper}>
-
+            <h1>HERO</h1>
         </div>
     )
 }
---
+++ components/heroData.js
@@ -0,0 +1,34 @@
+let data = [
+    {
+        imgSource: '/heroImages/Logitech-Aurora-Collection.JPG',
+        title: 'PLAY YOUR WAY',
+        description: 'MEET THE AURORA COLLECTION',
+        btnText: 'SHOP AND CUSTOMIZE NOW'
+    },
+    {
+        imgSource: '/heroImages/league.jpg',
+        title: 'PLAY WITH LEGENDS AND SAVE!',
+        description: 'Buy Any 2 x Leage of Legend Products and get 15% off, buy 3 and get 30% off',
+        btnText: 'EXPLORE LEAGE OF LEGENDS COLLECTION'
+    },
+    {
+        imgSource: '/heroImages/shroud.jpg',
+        title: 'AIM HIGHER',
+        description: 'G303 Shroud Edition. Designed with Shroud. Made for play.',
+        btnText: 'EXPLORE G303 SHROUD'
+    },
+    {
+        imgSource: '/heroImages/Opera.jpg',
+        title: 'SHROUD x LOGITECH G',
+        description: 'LOGITECH G PRO SERIES - SHROUD EDITION',
+        btnText: 'SHOP SHROUD EDITION'
+    },
+    {
+        imgSource: '/heroImages/headphones.jpg',
+        title: 'PLAY NEVER ENDS',
+        description: 'Play games. Play music. Play with friends.',
+        btnText: 'MEET G435'
+    }
+]
+
+export default data
