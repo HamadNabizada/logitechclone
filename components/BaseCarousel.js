8be74ff28e8d28099b9ab986ff88a66e7026b797
Hamad <HamadNabizada@gmail.com>
2022-08-09 16:20:59 -0700

    updated styling to be more responsive. added BaseCarousel component

---
+++ components/BaseCarousel.js
@@ -0,0 +1,10 @@
+import style from '../styles/BaseCarousel.module.css'
+
+
+export default function BaseCarousel(){
+    return(
+        <div className={style.wrapper}>
+            <h2>ADVANCED GAMING GEAR</h2>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,16 @@ import Header from '../components/Header'
 import style from '../styles/Index.module.css'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
+import BaseCrousel from '../components/BaseCarousel'
 
 export default function Index() {
 
   return (
     <div className={style.wrapper}>
       <Header />
-      <Hero />
       <Navbar />
+      <Hero />
+      <BaseCrousel />
     </div>
   )
 }
