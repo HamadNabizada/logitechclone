63399a058d247b01be7b8409804274930c1e433f
Hamad <HamadNabizada@gmail.com>
2022-08-09 16:39:45 -0700

    product info DB added

--- components/BaseCarousel.js
+++ components/BaseCarousel.js
@@ -1,10 +1,23 @@
 import style from '../styles/BaseCarousel.module.css'
 
 
-export default function BaseCarousel(){
+export default function BaseCarousel(props){
     return(
         <div className={style.wrapper}>
-            <h2>ADVANCED GAMING GEAR</h2>
+            <div className={style.text}>
+                <h2 className={style.title}>{props.title}</h2>
+                <p className={props.description}>{props.description}</p>
+            </div>
+            <div className={style.carousel}>
+                <div className={style.imgContainer}>
+                    <img src="#" alt="#" />
+                </div>
+                <div className={style.productInfo}>
+                    <h4>{props.product}</h4>
+                    <p>{props.productItem}</p>
+                    <p>{props.price}</p>
+                </div>
+            </div>
         </div>
     )
 }
---
+++ components/productData.js
@@ -0,0 +1,38 @@
+let products = [
+    {
+        imgSource: "/products/pro-x-keyboard-gallery-1.webp",
+        product: 'PRO X',
+        productItem: 'KEYBOARD',
+        price: '$149.99'
+    },
+    {
+        imgSource: "/products/plasma-hero-carbon-gallery-4.webp",
+        product: 'PRO GAMING MOUSE',
+        productItem: 'Gaming Mouse',
+        price: '$59.99'
+    },
+    {
+        imgSource: "/products/pro-headset-gallery-1.webp",
+        product: 'PRO HEADSET',
+        productItem: 'PRO Gaming Headset',
+        price: '$99.99'
+    },
+    {
+        imgSource: "/products/g840-gallery-1.webp",
+        product: 'G840',
+        productItem: 'XL Gaming Mouse Pad',
+        price: '$29.99-$49.99'
+    },
+    {
+        imgSource: "/products/x56-gallery-1.webp",
+        product: 'X56 H.O.T.A.S.',
+        productItem: 'RGB Throttle and Stick Simulation Controller',
+        price: '$249.99'
+    },
+    {
+        imgSource: "/products/pro-x-gallery-3.webp",
+        product: 'PRO X SWITCH KIT',
+        productItem: 'SWITCHES',
+        price: '$49.99'
+    }
+]
