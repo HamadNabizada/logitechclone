586cc1a44e54dc22964950bb0492365ffe267630
Hamad <HamadNabizada@gmail.com>
2022-08-10 19:56:00 -0700

    added gaminggear DB

---
+++ components/gamingGear.js
@@ -0,0 +1,62 @@
+let data = [
+    {
+        title: 'GAMING MICE',
+        imgSource: "/gear/mice.webp",
+        buttonTitle: 'SHOP MICE',
+        buttonLink: 'https://www.logitechg.com/en-us/products/gaming-mice.html'
+    },
+    {
+        title: 'GAMING MOUSE PADS',
+        imgSource: "/gear/mousepad.webp",
+        buttonTitle: 'SHOP MOUSE PADS',
+        buttonLink: 'https://www.logitechg.com/en-us/products/gaming-mouse-pads.html'
+    },
+    {
+        title: 'GAMING KEYBOARDS',
+        imgSource: "/gear/keyboard.webp",
+        buttonTitle: 'SHOP KEYBOARDS',
+        buttonLink: 'https://www.logitechg.com/en-us/products/gaming-keyboards.html'
+    },
+    {
+        title: 'GAMING AUDIO',
+        imgSource: "/gear/audio.webp",
+        buttonTitle: 'SHOP AUDIO',
+        buttonLink: 'https://www.logitechg.com/en-us/products/gaming-audio.html'
+    },
+    {
+        title: 'GAMING STREAMING GEAR',
+        imgSource: "/gear/streaming.webp",
+        buttonTitle: 'SHOP STREAMING GEAR',
+        buttonLink: 'https://www.logitechg.com/en-us/products/streaming-gear.html'
+    },
+    {
+        title: 'DRIVING',
+        imgSource: "/gear/driving.webp",
+        buttonTitle: 'SHOP DRIVING GEAR',
+        buttonLink: 'https://www.logitechg.com/en-us/products/driving.html'
+    },
+    {
+        title: 'SPACE',
+        imgSource: "/gear/space.webp",
+        buttonTitle: 'SHOP SPACE GEAR',
+        buttonLink: 'https://www.logitechg.com/en-us/products/space.html'
+    },
+    {
+        title: 'FLIGHT',
+        imgSource: "/gear/flight.webp",
+        buttonTitle: 'SHOP FLIGHT',
+        buttonLink: 'https://www.logitechg.com/en-us/products/flight.html'
+    },
+    {
+        title: 'FARM',
+        imgSource: "/gear/farm.webp",
+        buttonTitle: 'SHOP FARM GEAR',
+        buttonLink: 'https://www.logitechg.com/en-us/products/farm.html'
+    },
+    {
+        title: 'GAMEPADS',
+        imgSource: "/gear/gamepad.webp",
+        buttonTitle: 'SHOP GAMEPADS',
+        buttonLink: 'https://www.logitechg.com/en-us/products/gamepads.html'
+    }
+]
