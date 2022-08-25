869f11b8def000983dd76d71d7c995a5cc989706
Hamad <HamadNabizada@gmail.com>
2022-08-24 22:35:13 -0700

    updated logic for statement header animation

--- components/Statement.js
+++ components/Statement.js
@@ -1,6 +1,6 @@
 import style from '../styles/Statement.module.css'
 import { useEffect, useState, useRef } from 'react'
-import {nanoid} from 'nanoid'
+
 
 export default function Statement(){
     let [autoPlay, setAutoPlay] = useState(true)
@@ -10,14 +10,21 @@ export default function Statement(){
     useEffect(()=>{
         timeout = autoPlay && setTimeout(goUpArray, 4000)
     })
-    let statementsArray = [
-        <h4 key={nanoid()} ref={currentRef}>FREE SHIPPING ON ORDERS OVER $29</h4>,
-        <h4 key={nanoid()} ref={currentRef}>All products are certified carbon neutral</h4>,
-        <h4 key={nanoid()} ref={currentRef}>Donate to Ukraine Crisis Relief Fund</h4>
-    ]
     let [counter, setCounter] = useState(0)
-    
+    // let statementsArray = [
+    //     <h4 key={nanoid()} ref={currentRef}>FREE SHIPPING ON ORDERS OVER $29</h4>,
+    //     <h4 key={nanoid()} ref={currentRef}>All products are certified carbon neutral</h4>,
+    //     <h4 key={nanoid()} ref={currentRef}>Donate to Ukraine Crisis Relief Fund</h4>
+    // ]
+    let myStatement = <h4  ref={currentRef}>FREE SHIPPING ON ORDERS OVER $29</h4>
+    if(counter === 1){
+        myStatement = <h4  ref={currentRef}>All products are certified carbon neutral</h4>
+    }
+    if(counter === 2){
+        myStatement = <h4  ref={currentRef}>Donate to Ukraine Crisis Relief Fund</h4>
+    }
     function goUpArray(e){
+        console.log('rerender')
         currentRef.current.className=style.transitionRight
         setTimeout(()=>{
             currentRef.current.className=style.statement
@@ -25,7 +32,7 @@ export default function Statement(){
 
         setCounter( prevCounter=> {
             let newCounter = prevCounter + 1
-            if (statementsArray[newCounter] === undefined){
+            if (newCounter === 3){
                 newCounter = 0
             }
             return newCounter
@@ -38,7 +45,7 @@ export default function Statement(){
         },400)
         setCounter( prevCounter=> {
             let newCounter = prevCounter - 1
-            if (statementsArray[newCounter] === undefined){
+            if (newCounter === -1){
                 newCounter = 2
             }
             return newCounter
@@ -54,7 +61,7 @@ export default function Statement(){
     return (
         <div onMouseEnter={pauseSlide} onMouseLeave={resumeSlide} ref={statementRef} className={style.wrapper}>
             <div onClick={goDownArray} className={style.left}>&lsaquo;</div>
-            {statementsArray[counter]}
+            {myStatement}
             <div onClick={goUpArray} className={style.right}>&rsaquo;</div>
         </div>
     )
